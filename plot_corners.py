3d1d0c1dbdb289221a37793b21050d824476b161
Michael Hardman <29800382+mrhardman@users.noreply.github.com>
2025-07-31 14:39:57 +0100

    Get an array of cell vertices which has the data in a format close to that required by DMPlex.

--- plot_corners.py
+++ plot_corners.py
@@ -55,6 +55,57 @@ def remove_nonunique_points(corners_Rxy,corners_Zxy):
         corners_Zxy = np.delete(corners_Zxy,duplicate_indices)
     return corners_Rxy, corners_Zxy
 
+# identify which vertex an (R,Z) pair, or print error message
+def convert_R_Z_to_vertex_index(R,Z,R_vertices,Z_vertices):
+    Nc = len(R_vertices)
+    for ic in range(0,Nc):
+        if isapprox(R,R_vertices[ic]) and isapprox(Z,Z_vertices[ic]):
+            ic_vertex = ic
+            return ic_vertex
+    print("Failed to find (R,Z) index")
+    return None
+
+def get_cell_vertex_list_inner(R_ll,Z_ll, R_lr, Z_lr,
+                        R_ur, Z_ur, R_ul, Z_ul,
+                        R_vertices, Z_vertices):
+    vertices = []
+    # go in anti-clockwise order
+    vertices.append(convert_R_Z_to_vertex_index(R_ll,Z_ll,R_vertices,Z_vertices))
+    vertices.append(convert_R_Z_to_vertex_index(R_lr,Z_lr,R_vertices,Z_vertices))
+    vertices.append(convert_R_Z_to_vertex_index(R_ur,Z_ur,R_vertices,Z_vertices))
+    vertices.append(convert_R_Z_to_vertex_index(R_ul,Z_ul,R_vertices,Z_vertices))
+    return vertices
+
+def get_cell_vertex_list(Rxy_ll, Zxy_ll,
+        Rxy_lr, Zxy_lr, Rxy_ur, Zxy_ur, Rxy_ul, Zxy_ul,
+        R_vertices, Z_vertices):
+    Nx, Ny = np.shape(Rxy_ll)
+    Ncells = Nx*Ny
+    cell_vertices = np.zeros((Ncells,4),dtype=int)
+    cell_vertices_RZ = np.zeros((Ncells,4,2),dtype=float)
+    cell_vertices[:,:] = -3000
+    for ix in range(0,Nx):
+        for iy in range(0,Ny):
+            icell = iy + Ny*ix
+            R_ll = Rxy_ll[ix,iy]
+            Z_ll = Zxy_ll[ix,iy]
+            R_lr = Rxy_lr[ix,iy]
+            Z_lr = Zxy_lr[ix,iy]
+            R_ur = Rxy_ur[ix,iy]
+            Z_ur = Zxy_ur[ix,iy]
+            R_ul = Rxy_ul[ix,iy]
+            Z_ul = Zxy_ul[ix,iy]
+            cell_vertices[icell,:] = get_cell_vertex_list_inner(R_ll,Z_ll, R_lr, Z_lr,
+                        R_ur, Z_ur, R_ul, Z_ul,
+                        R_vertices, Z_vertices)
+            # this list must be in the same order as the list of cell vertices above
+            cell_vertices_RZ[icell,0,:] = [R_ll,Z_ll]
+            cell_vertices_RZ[icell,1,:] = [R_lr,Z_lr]
+            cell_vertices_RZ[icell,2,:] = [R_ur,Z_ur]
+            cell_vertices_RZ[icell,3,:] = [R_ul,Z_ul]
+    return cell_vertices, cell_vertices_RZ
+
+
 file_path = 'dmtest_data/expected_nonorthogonal.grd.nc'
 #file_path = 'dmtest_data/expected_orthogonal.grd.nc'
 dataset = nc.Dataset(file_path)
@@ -120,10 +171,24 @@ Zpoints_full = np.reshape(corners_Zxy, (Npoint_full,))
 # remove any duplicate points
 Rpoints_full, Zpoints_full = remove_nonunique_points(Rpoints_full,Zpoints_full)
 unique_points_1D(Rpoints_full,Zpoints_full)
-x = Rpoints
-y = Zpoints
+
+# get an array containing, for each cell, the list of vertices,
+# labelled by the global index defined implicitly by Rpoints_full, Zpoints_full
+# store this in `cell_vertices`, and store the RZ values of each vertex
+# cell_vertices[icell,ivertex] in R, Z = cell_vertices_RZ[icell,ivertex,:]
+cell_vertices, cell_vertices_RZ = get_cell_vertex_list(data_Rxy, data_Zxy,
+        data_Rxy_lr, data_Zxy_lr, data_Rxy_ur, data_Zxy_ur,
+        data_Rxy_ul, data_Zxy_ul, Rpoints_full, Zpoints_full)
+ncells, nvertices = np.shape(cell_vertices)
+for icell in range(0,ncells):
+    print(cell_vertices[icell,:])
+    print(cell_vertices_RZ[icell,:,:])
+
+
 # Make a scatter plot to show the mesh corners
 plt.figure(figsize=(10, 6))
+x = Rpoints
+y = Zpoints
 scatter = plt.scatter(x, y, c='b',marker='1')
 scatter = plt.scatter(Rpoints_lr, Zpoints_lr, c='r',marker='2')
 scatter = plt.scatter(Rpoints_ur, Zpoints_ur, c='g',marker='3')
