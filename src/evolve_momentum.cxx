7c9a0e380d044a78b87cb573ae63931c155b9703
David Bold <dave@ipp.mpg.de>
2025-08-21 10:13:22 +0200

    Remove withoutParallelSlices calls

--- src/evolve_momentum.cxx
+++ src/evolve_momentum.cxx
@@ -73,8 +73,7 @@ EvolveMomentum::EvolveMomentum(std::string name, Options &alloptions, Solver *so
     const auto coord = mesh->getCoordinates();
     // Note: This is 1 for a Clebsch coordinate system
     //       Remove parallel slices before operations
-    bracket_factor = sqrt(coord->g_22.withoutParallelSlices())
-      / (coord->J.withoutParallelSlices() * coord->Bxy);
+    bracket_factor = sqrt(coord->g_22) / (coord->J * coord->Bxy);
   } else {
     // Clebsch coordinate system
     bracket_factor = 1.0;
