53d84abf7a87072c4db78b2e13054351149bfac2
totork <ttork96@gmail.com>
2025-10-28 13:10:47 +0100

    More unit tests for diamagnetic_fci

--- tests/unit/test_diamagnetic_drift_fci.cxx
+++ tests/unit/test_diamagnetic_drift_fci.cxx
@@ -194,24 +194,7 @@ TEST_F(DiamagneticDriftFCITest, NoGradientB) {
 }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-TEST_F(DiamagneticDriftFCITest, TwoSpeciesDifferentCharge) {
+TEST_F(DiamagneticDriftFCITest, DifferentCharge) {
   Options options;
   options["units"]["meters"] = 1.0;
   options["units"]["seconds"] = 1.0;
@@ -246,22 +229,103 @@ TEST_F(DiamagneticDriftFCITest, TwoSpeciesDifferentCharge) {
   state["species"]["h++"]["momentum"] = N*V;
 
 
+  state["species"]["h++++"]["density"] = N;
+  state["species"]["h++++"]["temperature"] = T;
+  state["species"]["h++++"]["pressure"] = N*T;
+  state["species"]["h++++"]["charge"] = 4;
+  state["species"]["h++++"]["velocity"] = V;
+  state["species"]["h++++"]["momentum"] = N*V;
+
+
+
+
+  
   component.transform(state);
 
   Field3D ds1 = get<Field3D>(state["species"]["h+"]["density_source"]);
   Field3D ds2 = get<Field3D>(state["species"]["h++"]["density_source"]);
+  Field3D ds3 = get<Field3D>(state["species"]["h++++"]["density_source"]);
+
 
   Field3D ms1 = get<Field3D>(state["species"]["h+"]["momentum_source"]);
   Field3D ms2 = get<Field3D>(state["species"]["h++"]["momentum_source"]);
-
+  Field3D ms3 = get<Field3D>(state["species"]["h++++"]["momentum_source"]);
+  
   Field3D es1 = get<Field3D>(state["species"]["h+"]["energy_source"]);
   Field3D es2 = get<Field3D>(state["species"]["h++"]["energy_source"]);
-
+  Field3D es3 = get<Field3D>(state["species"]["h++++"]["energy_source"]);
 
 
   BOUT_FOR_SERIAL(i, ds1.getRegion("RGN_NOBNDRY")) {
     ASSERT_DOUBLE_EQ(ds1[i],2.0*ds2[i]);
     ASSERT_DOUBLE_EQ(ms1[i],2.0*ms2[i]);
     ASSERT_DOUBLE_EQ(es1[i],2.0*es2[i]);
+
+    ASSERT_DOUBLE_EQ(ds1[i],4.0*ds3[i]);
+    ASSERT_DOUBLE_EQ(ms1[i],4.0*ms3[i]);
+    ASSERT_DOUBLE_EQ(es1[i],4.0*es3[i]);
+    
   }
 }
+
+
+
+
+TEST_F(DiamagneticDriftFCITest, DoubleGradient) {
+  Options options;
+  options["units"]["meters"] = 1.0;
+  options["units"]["seconds"] = 1.0;
+  options["units"]["Tesla"] = 1.0;
+
+
+  Field3D B = FieldFactory::get()->create3D("1 + x", &options, mesh);
+  mesh->getCoordinates()->Bxy = B;
+
+
+  DiamagneticDriftFCI component("h+", options, nullptr);
+
+  Field3D N = FieldFactory::get()->create3D("1 + y * (x - 0.5)", &options, mesh);
+  Field3D T = FieldFactory::get()->create3D("1 + 0.2 * x + sin(z)", &options, mesh);
+  Field3D V = FieldFactory::get()->create3D("1 + 1.3 * x + cos(2.0*z)", &options, mesh);
+  mesh->communicate(N,V,T);
+
+  Options state;
+  state["species"]["h1"]["density"] = N;
+  state["species"]["h1"]["temperature"] = T;
+  state["species"]["h1"]["pressure"] = N*T;
+  state["species"]["h1"]["charge"] = 1;
+  state["species"]["h1"]["velocity"] = V;
+  state["species"]["h1"]["momentum"] = N*V;
+
+  // Multiply density by two to get the difference in gradient, then check if the diamagnetic drift scales accordingly
+  state["species"]["h2"]["density"] = 2.0 * N;
+  state["species"]["h2"]["temperature"] = T;
+  state["species"]["h2"]["pressure"] = 2.0 * N * T;
+  state["species"]["h2"]["charge"] = 1;
+  state["species"]["h2"]["velocity"] = V;
+  state["species"]["h2"]["momentum"] = 2.0 * N * V;
+
+  component.transform(state);
+
+  Field3D ds1 = get<Field3D>(state["species"]["h1"]["density_source"]);
+  Field3D ds2 = get<Field3D>(state["species"]["h2"]["density_source"]);
+
+
+  Field3D ms1 = get<Field3D>(state["species"]["h1"]["momentum_source"]);
+  Field3D ms2 = get<Field3D>(state["species"]["h2"]["momentum_source"]);
+
+
+  Field3D es1 = get<Field3D>(state["species"]["h1"]["energy_source"]);
+  Field3D es2 = get<Field3D>(state["species"]["h2"]["energy_source"]);
+
+
+
+  BOUT_FOR_SERIAL(i, ds1.getRegion("RGN_NOBNDRY")) {
+    
+    ASSERT_DOUBLE_EQ(ds1[i],0.5*ds2[i]);
+    ASSERT_DOUBLE_EQ(ms1[i],0.5*ms2[i]);
+    ASSERT_DOUBLE_EQ(es1[i],0.5*es2[i]);
+
+  }
+}
+
