e4057e1cb501780a1005167db89ada3f0ccefcd0
Chris MacMackin <chris.macmackin@ukaea.uk>
2025-11-04 15:48:54 +0000

    Specify which variables are read/written for reaction components

--- src/ionisation.cxx
+++ src/ionisation.cxx
@@ -2,7 +2,15 @@
 #include "../include/ionisation.hxx"
 #include "../include/integrate.hxx"
 
-Ionisation::Ionisation(std::string name, Options &alloptions, Solver *) {
+Ionisation::Ionisation(std::string name, Options& alloptions, Solver*)
+    : Component(
+        {readOnly("species:h:density"), readOnly("species:h:temperature"),
+         readOnly("species:h:velocity"), readOnly("species:h:AA"),
+         readOnly("species:e:density"), readOnly("species:e:temperature"),
+         readOnly("species:h+:AA"), readWrite("species:h:density_source"),
+         readWrite("species:h+:density_source"), readWrite("species:h:momentum_source"),
+         readWrite("species:h+:momentum_source"), readWrite("species:h:energy_source"),
+         readWrite("species:h+:energy_source"), readWrite("species:e:energy_source")}) {
 
   // Get options for this component
   auto& options = alloptions[name];
