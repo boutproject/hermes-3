1deab56742d34deebd0abfe05e7866f2a133d629
Chris MacMackin <chris.macmackin@ukaea.uk>
2025-11-12 14:16:37 +0000

    Added access permissions for remaining components
    All unit and integration tests pass, but many components are
    untested. There may be errors in their permissions.

--- src/evolve_momentum.cxx
+++ src/evolve_momentum.cxx
@@ -63,7 +63,7 @@ EvolveMomentum::EvolveMomentum(std::string name, Options& alloptions, Solver* so
   momentum_source = 0.0;
 
   state_variable_access.substitute("name", {name});
-  state_variable_access.substitute("inputs", {"velocity", "momentum"});
+  state_variable_access.substitute("outputs", {"velocity", "momentum"});
 }
 
 void EvolveMomentum::transform_impl(GuardedOptions& state) {
