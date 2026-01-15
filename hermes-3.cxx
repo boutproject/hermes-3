b3bb34899ae613a76292b9f7edf5d17c91d480ec
malamast <malamas.tsagkaridis@gmail.com>
2026-01-15 15:55:37 -0800

    parallel_inertia_curvature_drift: added a new component for including the ~Vpar^2 curvature drift
    This term comes from the parallel inertia term in the momentum equation.
    
    It is due to parallel inertia in a curved magnetic field (centrifugal-like force).
    
    See ~Vpar^2 term in the paper by in A.V. Chankin, Journal of Nuclear Materials 241-243 (1997) 199-213

--- hermes-3.cxx
+++ hermes-3.cxx
@@ -59,6 +59,7 @@
 #include "include/neutral_mixed.hxx"
 #include "include/neutral_parallel_diffusion.hxx"
 #include "include/noflow_boundary.hxx"
+#include "include/parallel_inertia_curvature_drift.hxx"
 #include "include/parallel_ohms_law.hxx"
 #include "include/polarisation_drift.hxx"
 #include "include/quasineutral.hxx"
