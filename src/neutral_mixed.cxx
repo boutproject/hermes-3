65c1a13c755fa205819da46d17af620b5e9d1510
totork <ttork96@gmail.com>
2026-01-30 10:43:40 +0100

    Fix bug operation on empty data

--- src/neutral_mixed.cxx
+++ src/neutral_mixed.cxx
@@ -394,9 +394,9 @@ void NeutralMixed::finally(const Options& state) {
       }
     }
     // The factor here is likely 3/2 as this is pure energy flow, but needs checking.                                                                                                                             
-    ef_cond_perp_xlow *= 3/2;
-    ef_cond_perp_ylow *= 3/2;
-    ef_cond_par_ylow *= 3/2;
+    //ef_cond_perp_xlow *= 3/2;
+    //ef_cond_perp_ylow *= 3/2;
+    //ef_cond_par_ylow *= 3/2;
   }
   
   Sp = pressure_source;
