731af4ad5d9e28e517c40e59694ce85a86821a7a
totork <ttork96@gmail.com>
2026-02-11 09:13:42 +0100

    Add grid-dependend dissipation of vorticity to localize dissipation in the core

--- include/relax_potential.hxx
+++ include/relax_potential.hxx
@@ -87,7 +87,9 @@ private:
   }
   Field3D logB;
   Field3D viscosity; /// Kinematic viscosity
+  Field3D viscosity_core;
   Field3D viscosity_par;
+  Field3D ones;
   bool phi_dissipation; /// Parallel dissipation of potential
   BoutReal vort_timedissipation;
   
