da05cb32b83e876d13cdd0d409165a3d844fed59
Ben Dudson <dudson2@llnl.gov>
2026-04-10 10:59:49 -0700

    braginskii_friction: Don't need collision frequencies in boundaries
    Use GET_NOBOUNDARY rather than GET_VALUE, since collision frequencies
    are not needed (or valid) in the boundaries.

--- src/braginskii_friction.cxx
+++ src/braginskii_friction.cxx
@@ -104,7 +104,7 @@ void BraginskiiFriction::transform_impl(GuardedOptions& state) {
         continue;
       }
 
-      const Field3D nu = GET_VALUE(Field3D, species1["collision_frequencies"][coll_name]);
+      const Field3D nu = GET_NOBOUNDARY(Field3D, species1["collision_frequencies"][coll_name]);
       const Field3D density2 = GET_NOBOUNDARY(Field3D, species2["density"]);
       const Field3D velocity2 = species2.isSet("velocity")
                                     ? GET_NOBOUNDARY(Field3D, species2["velocity"])
