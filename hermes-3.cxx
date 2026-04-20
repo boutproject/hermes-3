dfb01ff172187030422efbbcb736fbd03938d51b
totork <ttork96@gmail.com>
2026-04-20 18:55:48 +0200

    Add metric normalisation for Fci, throw error if recalcuated while Fci.

--- hermes-3.cxx
+++ hermes-3.cxx
@@ -224,6 +224,10 @@ int Hermes::init(bool restarting) {
           .withDefault(false)) {
     recalculate_metric(rho_s0, Bnorm);
 
+    if (mesh->isFci()){
+      throw BoutException("Recalculating metric does (probably) not work for Fci!");
+    }
+    
   } else {
     // Check that the grid file contains at least one metric tensor component
     // Note: Older grid files did not, so would silently default to the identity metric
@@ -249,28 +253,51 @@ int Hermes::init(bool restarting) {
              .doc("Normalise input metric tensor? (assumes input is in SI units)")
              .withDefault<bool>(true)) {
       Coordinates *coord = mesh->getCoordinates();
-      // To use non-orthogonal metric
-      // Normalise
-      coord->dx /= rho_s0 * rho_s0 * Bnorm;
-      coord->Bxy /= Bnorm;
-      // Metric is in grid file - just need to normalise
-      coord->g11 /= SQ(Bnorm * rho_s0);
-      coord->g22 *= SQ(rho_s0);
-      coord->g33 *= SQ(rho_s0);
-      coord->g12 /= Bnorm;
-      coord->g13 /= Bnorm;
-      coord->g23 *= SQ(rho_s0);
+      if (mesh->isFci()){
+	// Normalise metric for Fci
+	coord->g11 *= SQ(rho_s0);
+	coord->g22 *= SQ(rho_s0);
+	coord->g33 *= SQ(rho_s0);
+	coord->g12 *= SQ(rho_s0);
+	coord->g13 *= SQ(rho_s0);
+	coord->g23 *= SQ(rho_s0);
 
-      coord->J *= Bnorm / rho_s0;
+	coord->J /= rho_s0 * rho_s0 * rho_s0;
 
-      coord->g_11 *= SQ(Bnorm * rho_s0);
-      coord->g_22 /= SQ(rho_s0);
-      coord->g_33 /= SQ(rho_s0);
-      coord->g_12 *= Bnorm;
-      coord->g_13 *= Bnorm;
-      coord->g_23 /= SQ(rho_s0);
+	coord->g_11 /= SQ(rho_s0);
+	coord->g_22 /= SQ(rho_s0);
+	coord->g_33 /= SQ(rho_s0);
+	coord->g_12 /= SQ(rho_s0);
+	coord->g_13 /= SQ(rho_s0);
+	coord->g_23 /= SQ(rho_s0);
 
-      coord->geometry(); // Calculate other metrics
+	coord->Bxy /= Bnorm;
+
+	coord->geometry(); // Calculate other metrics	
+      } else {
+	// To use non-orthogonal metric
+	// Normalise
+	coord->dx /= rho_s0 * rho_s0 * Bnorm;
+	coord->Bxy /= Bnorm;
+	// Metric is in grid file - just need to normalise
+	coord->g11 /= SQ(Bnorm * rho_s0);
+	coord->g22 *= SQ(rho_s0);
+	coord->g33 *= SQ(rho_s0);
+	coord->g12 /= Bnorm;
+	coord->g13 /= Bnorm;
+	coord->g23 *= SQ(rho_s0);
+
+	coord->J *= Bnorm / rho_s0;
+
+	coord->g_11 *= SQ(Bnorm * rho_s0);
+	coord->g_22 /= SQ(rho_s0);
+	coord->g_33 /= SQ(rho_s0);
+	coord->g_12 *= Bnorm;
+	coord->g_13 *= Bnorm;
+	coord->g_23 /= SQ(rho_s0);
+
+	coord->geometry(); // Calculate other metrics
+      }
     }
   }
 
