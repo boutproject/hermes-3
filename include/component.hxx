d255141508b4f26e97189350f810fe320efe4c16
Ben Dudson <dudson2@llnl.gov>
2026-07-05 12:48:46 -0700

    component: Small fixes to component formatting
    Advance formatter on unknown character
    
    Handle trailing `~` without dereferencing `end`.

--- include/component.hxx
+++ include/component.hxx
@@ -601,11 +601,12 @@ struct fmt::formatter<Component> : formatter<string_view> {
     }
 
     while (it != end and *it != ':' and *it != '}') {
-      // Other cases handled explicitly below
-      // NOLINTNEXTLINE(bugprone-switch-missing-default-case)
       switch (*it) {
       case '~':
         ++it;
+        if (it == end) {
+          throw fmt::format_error("Unrecognised format specifier '~'");
+        }
         if (*it == 'n') {
           hide_name = true;
         } else if (*it == 't') {
@@ -620,10 +621,10 @@ struct fmt::formatter<Component> : formatter<string_view> {
         show_type = true;
         ++it;
         break;
+      default:
+        ++it;
+        break;
       }
-      // FIXME: This is copied from the parser used for Fields, but
-      // won't it result in an infinite loop if an unrecognised
-      // character is encountered?
     }
 
     if (hide_type and show_type) {
