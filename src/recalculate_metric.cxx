e0e62a0acf985e17c308869422058ad066ee0792
David Bold <dave@ipp.mpg.de>
2026-08-17 14:26:05 +0200

    Fix recalculate_metric for 3D metrics compilation

--- src/recalculate_metric.cxx
+++ src/recalculate_metric.cxx
@@ -45,22 +45,23 @@ void recalculate_metric(BoutReal Lnorm, BoutReal Bnorm) {
     sbp = -1.0;
   }
 
-  const auto g11 = SQ(Rxy * Bpxy);
-  const auto g22 = 1.0 / SQ(hthe);
+  const Field2D g11 = SQ(Rxy * Bpxy);
+  const Field2D g22 = 1.0 / SQ(hthe);
   const auto g33 = SQ(sinty) * g11 + SQ(coord->Bxy()) / g11;
-  const auto g12 = 0.0;
-  const auto g13 = -sinty * g11;
-  const auto g23 = -sbp * Btxy / (hthe * Bpxy * Rxy);
+  const Field2D g12 = 0.0;
+  const Field2D g13 = -sinty * g11;
+  const Field2D g23 = -sbp * Btxy / (hthe * Bpxy * Rxy);
 
-  const auto g_11 = 1.0 / g11 + SQ(sinty * Rxy);
+  const Field2D g_11 = 1.0 / g11 + SQ(sinty * Rxy);
   const auto g_22 = SQ(coord->Bxy() * hthe / Bpxy);
-  const auto g_33 = Rxy * Rxy;
-  const auto g_12 = sbp * Btxy * hthe * sinty * Rxy / Bpxy;
-  const auto g_13 = sinty * Rxy * Rxy;
-  const auto g_23 = sbp * Btxy * hthe * Rxy / Bpxy;
+  const Field2D g_33 = Rxy * Rxy;
+  const Field2D g_12 = sbp * Btxy * hthe * sinty * Rxy / Bpxy;
+  const Field2D g_13 = sinty * Rxy * Rxy;
+  const Field2D g_23 = sbp * Btxy * hthe * Rxy / Bpxy;
 
   coord->setMetricTensor(ContravariantMetricTensor(g11, g22, g33, g12, g13, g23),
                          CovariantMetricTensor(g_11, g_22, g_33, g_12, g_13, g_23));
 
-  coord->setJ(hthe / Bpxy);
+  const Field2D J = hthe / Bpxy;
+  coord->setJ(J);
 }
