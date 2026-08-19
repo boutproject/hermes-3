898bf62454ca7b00b1ddbe269f1b2c9cd9ef4e23
David Bold <dave@ipp.mpg.de>
2026-08-19 09:35:11 +0200

    mms is in solver section

--- src/evolve_momentum.cxx
+++ src/evolve_momentum.cxx
@@ -71,7 +71,7 @@ EvolveMomentum::EvolveMomentum(std::string name, Options& alloptions, Solver* so
   use_div_par_fvv = options["use_div_par_fvv"]
                         .doc("Use Div_par_fvv instead of Div_par\nOnly for MMS tests")
                         .withDefault<bool>(use_div_par_fvv);
-  if ((not use_div_par_fvv) and (not alloptions["mms"].as<bool>())) {
+  if ((not use_div_par_fvv) and (not alloptions["solver"]["mms"].as<bool>())) {
     throw BoutException("use_div_par_fvv is only for MMS tests");
   }
 
