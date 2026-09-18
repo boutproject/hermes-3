f9f120361efccbb075a7a5a0313ff3e97b2d129c
totork <ttork96@gmail.com>
2026-09-18 13:55:34 +0200

    Change F3DP to F3D as it does not need parallel slices

--- include/braginskii_ion_viscosity.hxx
+++ include/braginskii_ion_viscosity.hxx
@@ -74,8 +74,8 @@ private:
     Field3D nu_star;
   };
 
-  Field3DParallel Bxy, sqrtB, Grad_par_logB; ///< Magnetic field variables
-
+  Field3DParallel Bxy, sqrtB; ///< Magnetic field variables
+  Field3D Grad_par_logB;
   /// Store diagnostics for each species
   std::map<std::string, Diagnostics> diagnostics;
 
