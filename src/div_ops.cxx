82852472f3eeef05f7d2af547338479f9902f3eb
totork <ttork96@gmail.com>
2026-09-24 23:11:17 +0200

    Use arakawa bracket when Fci for ExB

--- src/div_ops.cxx
+++ src/div_ops.cxx
@@ -118,6 +118,11 @@ void MC(Stencil1D& n) {
  */
 Field3D Div_n_bxGrad_f_B_XPPM(const Field3D& n, const Field3D& f, bool bndry_flux,
                               bool poloidal, bool positive) {
+
+  if (n.isFci()) {
+    return -bracket(n, f, BRACKET_ARAKAWA);
+  }
+
   Field3D result{0.0};
 
   Coordinates* coord = mesh->getCoordinates();
